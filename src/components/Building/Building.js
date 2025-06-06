bd41038e20cfefac9f179e73303a3db7d7bd77ce
Private Use <0x92dev@gmail.com>
2025-06-06 15:55:40 +0200

    fix asset paths

--- src/components/Building/Building.js
+++ src/components/Building/Building.js
@@ -9,9 +9,9 @@ const Building = () => {
         <>
             <Wall 
                 position={[0, 0, -13.5]}
-                modelUrl={"assets/3D/Wall/scene.gltf"}
-                mapUrl={"assets/3D/Wall/Textures/White_Wall.jpg"}
-                normalMapUrl={"assets/3D/Wall/Textures/White_Wall_NORMAL.jpg"}
+                modelUrl={process.env.PUBLIC_URL + "/assets/3D/Wall/scene.gltf"}
+                mapUrl={process.env.PUBLIC_URL + "/assets/3D/Wall/Textures/White_Wall.jpg"}
+                normalMapUrl={process.env.PUBLIC_URL + "/assets/3D/Wall/Textures/White_Wall_NORMAL.jpg"}
             />
 
             {/* side windows */}
@@ -19,27 +19,27 @@ const Building = () => {
                 scale={[0.008, 0.008, 0.008]}
                 position={[6.5, 8.5, -15]}
                 rotation={[0, Math.PI ,0]}
-                modelUrl={"assets/3D/WindowNoGlassL/scene.gltf"}
-                mapUrl={"assets/3D/WindowNoGlassL/Textures/Material_49_baseColor.png"}
+                modelUrl={process.env.PUBLIC_URL + "/assets/3D/WindowNoGlassL/scene.gltf"}
+                mapUrl={process.env.PUBLIC_URL + "/assets/3D/WindowNoGlassL/Textures/Material_49_baseColor.png"}
             />
             <WindowFrame 
                 scale={[0.008, 0.008, 0.008]}
                 position={[-6.5, 8.5, -15]}
                 rotation={[0, Math.PI ,0]}
-                modelUrl={"assets/3D/WindowNoGlassR/scene.gltf"}
-                mapUrl={"assets/3D/WindowNoGlassR/Textures/Material_49_baseColor.png"}
+                modelUrl={process.env.PUBLIC_URL + "/assets/3D/WindowNoGlassR/scene.gltf"}
+                mapUrl={process.env.PUBLIC_URL + "/assets/3D/WindowNoGlassR/Textures/Material_49_baseColor.png"}
             />
             <Glass            
                 scale={[0.008, 0.008, 0.008]}
                 position={[6.5, 8.5, -15]}
                 rotation={[0, 0, 0]}
-                url={"assets/3D/WindowGlassL/scene.gltf"}                        
+                url={process.env.PUBLIC_URL + "/assets/3D/WindowGlassL/scene.gltf"}                        
             />
             <Glass            
                 scale={[0.008, 0.008, 0.008]}
                 position={[-6.5, 8.5, -15]}
                 rotation={[0, 0, 0]}
-                url={"assets/3D/WindowGlassR/scene.gltf"}                        
+                url={process.env.PUBLIC_URL + "/assets/3D/WindowGlassR/scene.gltf"}                        
             />
 
             {/* roof */}
@@ -47,14 +47,14 @@ const Building = () => {
                 scale={[2.7, 2.7, 2.7]}
                 position={[0, 27, 13.2]}
                 rotation={[0, 0, 0]}
-                modelUrl={"assets/3D/RoofNoGlass/scene.gltf"}
-                mapUrl={"assets/3D/RoofNoGlass/Textures/Material_49_baseColor.png"}
+                modelUrl={process.env.PUBLIC_URL + "/assets/3D/RoofNoGlass/scene.gltf"}
+                mapUrl={process.env.PUBLIC_URL + "/assets/3D/RoofNoGlass/Textures/Material_49_baseColor.png"}
             />
             <Glass            
                 scale={[2.7, 2.7, 2.7]}
                 position={[0, 27, 13.2]}
                 rotation={[0, 0, 0]}
-                url={"assets/3D/RoofGlass/scene.gltf"}                        
+                url={process.env.PUBLIC_URL + "/assets/3D/RoofGlass/scene.gltf"}                        
             />
         </>
 
